c83cf709f65f81b03d76010894df1b6704ff9f38
Brenden Hartman <brendenhart22@gmail.com>
2024-05-02 11:34:27 -0500

    worked making my own stack data structure

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,33 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) <= self.limit:
+            self.items.append(item)
+        else:
+            raise Exception()
 
     def pop(self):
-        pass
+        if len(self.items) != 0:
+            self.items.pop()
+            return 1
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - self.items.index(target) - 1
+        return -1
